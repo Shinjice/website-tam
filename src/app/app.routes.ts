74d1bccbfd88771903b6bea8ab766522176bd196
Tam Desktop <Tam@brandmannen.nl>
2025-02-20 22:49:31 +0100

    added home, navbar, changes routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home',
+        component: HomeComponent,
+        children: [
+          {
+            path: 'Home',
+            loadChildren: () =>
+              import('./modules/home/home.module').then(m => m.HomeModule)
+          },
+        ]
+    }
+];
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    NavbarComponent
+  ]
+})
+export class HomeModule { }
